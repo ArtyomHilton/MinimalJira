b7e967837dcfc8f9da3ecfe7593c70f5a6231382
HILTON <artyomhilton@yahoo.com>
2025-11-16 02:34:08 +0300

    Add caching

--- Backend/src/MinimalJira.Application/UseCases/Project/GetProjects/GetProjectsUseCase.cs
+++ Backend/src/MinimalJira.Application/UseCases/Project/GetProjects/GetProjectsUseCase.cs
@@ -1,18 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using MinimalJira.Application.DTOs;
+using MinimalJira.Application.Interfaces;
 using MinimalJira.Persistence.Context;
 
 namespace MinimalJira.Application.UseCases.Project.GetProjects;
 
-public class GetProjectsUseCase(MinimalJiraDbContext dbContext, ILogger<GetProjectsUseCase> logger) : IGetProjectsUseCase
+public class GetProjectsUseCase(
+    MinimalJiraDbContext dbContext,
+    ICacheService cacheService,
+    ILogger<GetProjectsUseCase> logger) : IGetProjectsUseCase
 {
     public async Task<ICollection<ProjectDataResponse>> ExecuteAsync(GetProjectsQuery query,
         CancellationToken cancellationToken)
     {
-        logger.LogInformation("Началось получение проектов на страницу {Page} в количестве {PageSize}", query.Page, query.PageSize);
-        
-        return await dbContext.Projects
+        logger.LogInformation("Началось получение проектов на страницу {Page} в количестве {PageSize}", query.Page,
+            query.PageSize);
+
+        var key = $"projects_page_{query.Page}_pageSize_{query.PageSize}";
+
+        var cacheProjects = await cacheService.GetAsync<ICollection<ProjectDataResponse>>(key, cancellationToken);
+
+        if (cacheProjects is not null)
+        {
+            return cacheProjects;
+        }
+
+        var projects = await dbContext.Projects
             .Include(p => p.Tasks)
             .Select(p => new ProjectDataResponse
             {
@@ -29,9 +43,13 @@ public class GetProjectsUseCase(MinimalJiraDbContext dbContext, ILogger<GetProje
                     IsComplete = t.IsComplete
                 }).ToList()
             })
-            .OrderBy(x=> x.CreatedAt)
+            .OrderBy(x => x.CreatedAt)
             .Skip((query.Page - 1) * query.PageSize)
             .Take(query.PageSize)
             .ToListAsync(cancellationToken);
+
+        await cacheService.SetAsync(key, projects, 5, cancellationToken);
+
+        return projects;
     }
 }
