72d7a32c483862ff1965437f024aaf36f2ee0f18
HILTON <artyomhilton@yahoo.com>
2025-11-16 13:41:39 +0300

    Add AsNoTracking for get query

--- Backend/src/MinimalJira.Application/UseCases/Project/GetProjectById/GetProjectByIdUseCase.cs
+++ Backend/src/MinimalJira.Application/UseCases/Project/GetProjectById/GetProjectByIdUseCase.cs
@@ -6,12 +6,13 @@ using MinimalJira.Persistence.Context;
 
 namespace MinimalJira.Application.UseCases.Project.GetProjectById;
 
-public class GetProjectByIdUseCase(MinimalJiraDbContext dbContext, ILogger<GetProjectByIdUseCase> logger) : IGetProjectByIdUseCase
+public class GetProjectByIdUseCase(MinimalJiraDbContext dbContext, ILogger<GetProjectByIdUseCase> logger)
+    : IGetProjectByIdUseCase
 {
     public async Task<ProjectDataResponse> ExecuteAsync(GetProjectByIdQuery query, CancellationToken cancellationToken)
     {
         logger.LogInformation("Началось получение проекта с Id: {Id}", query.Id);
-        
+
         var project = await dbContext.Projects
             .Include(p => p.Tasks)
             .Select(p => new ProjectDataResponse
@@ -28,15 +29,17 @@ public class GetProjectByIdUseCase(MinimalJiraDbContext dbContext, ILogger<GetPr
                     CreatedAt = t.CreatedAt,
                     IsComplete = t.IsComplete
                 }).ToList()
-            }).FirstOrDefaultAsync(p => p.Id == query.Id, cancellationToken);
+            }).AsNoTracking()
+            .FirstOrDefaultAsync(p => p.Id == query.Id, cancellationToken);
+        
         if (project is null)
         {
             logger.LogInformation("Не удалось найти проект с Id: {Id}", query.Id);
             throw new NotFoundException($"Проект c Id: {query.Id} не найден!");
         }
 
-        logger.LogInformation("Проект с Id: {Id} успешно найден", query.Id);        
-        
+        logger.LogInformation("Проект с Id: {Id} успешно найден", query.Id);
+
         return project;
     }
 }
